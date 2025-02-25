df68e114d6d7a22bf1b642e048528dea92e89ba5
zyx <2511305125@qq.com>
2025-02-25 17:20:06 +0000

    DEBUG

--- include/motor_control.h
+++ include/motor_control.h
@@ -26,7 +26,7 @@ public:
         time_flag = 1;
         count = 0;
         angle.PrevData = 0;
-        timer_1s.start(1000, [&]()
+        timer_1s.start(3000, [&]()
                        { time_flag = 1; });
         if (motor.start(0, 17, 27, 22, 5))
         {
