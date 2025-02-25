5c1e7e543ed75002b27fcf6aad114ad5b7182f5b
zyx <2511305125@qq.com>
2025-02-25 17:04:04 +0000

    Add motor movement

--- src/motor_control.cpp
+++ src/motor_control.cpp
@@ -1,4 +1,5 @@
 #include "motor_control.h"
+#include <cmath>
 
 void MotorControl::onSensorData(float value)
 {
@@ -11,6 +12,19 @@ void MotorControl::onSensorData(float value)
         angle.PrevData = angle.NewData;
 
         std::cout << "Angle Change: " << angle.RevData <<" New Angle: " << angle.NewData<< "°\n";
+
+        float steps = angle.RevData /5.625;
+        int intSteps = static_cast<int>(round(steps));
+
+        if(intSteps > 0)
+        {
+            motor.forward(intSteps);
+        }
+        else if(intSteps < 0)
+        {
+            motor.backward(-intSteps);
+        }
+
         time_flag = 0;
     }
 }
