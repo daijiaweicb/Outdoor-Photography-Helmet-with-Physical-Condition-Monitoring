a5374ad849a59999144028cd3a948d356ed2b5b3
zyx <2511305125@qq.com>
2025-02-25 16:54:09 +0000

    DEBUG

--- src/motor_control.cpp
+++ src/motor_control.cpp
@@ -2,16 +2,15 @@
 
 void MotorControl::onSensorData(float value)
 {
-    std::lock_guard<std::mutex> lock(motor_mutex);
+    // std::lock_guard<std::mutex> lock(motor_mutex);
 
-    
     if (time_flag == 1)
     {
         angle.NewData = value;
         angle.RevData = angle.NewData - angle.PrevData;
         angle.PrevData = angle.NewData;
 
-        std::cout << "Angle Change: " << angle.RevData << "°\n";
+        std::cout << "Angle Change: " << angle.RevData <<"New Angle: " << angle.NewData<< "°\n";
         time_flag = 0;
     }
 }
