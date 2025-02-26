9fb3185234d82f4178824d9fc016dfab49e6f64f
DUIDEDUIDEDUIMAABUDUI <enecholighten@163.com>
2025-02-26 00:40:31 +0000

    pid6

--- src/motor_control.cpp
+++ src/motor_control.cpp
@@ -9,41 +9,41 @@ void MotorControl::onSensorData(float value)
 
     // std::lock_guard<std::mutex> lock(motor_mutex);
     angle.NewData = value;
-    if (time_flag == 1)
-    {
+    // if (time_flag == 1)
+    // {
         
 
-    angle.target_angle += (angle.NewData - angle.PrevData);
+    // angle.target_angle += (angle.NewData - angle.PrevData);
  
  
         
     
-    float error = angle.target_angle - angle.NewData;
-    angle.integral += error;
-    float derivative = error - angle.prev_error;
-    angle.prev_error = error;
-    float output = angle.Kp * error + angle.Ki * angle.integral + angle.Kd * derivative;
+    // float error = angle.target_angle - angle.NewData;
+    // angle.integral += error;
+    // float derivative = error - angle.prev_error;
+    // angle.prev_error = error;
+    // float output = angle.Kp * error + angle.Ki * angle.integral + angle.Kd * derivative;
 
-    std::cout << "PID Output: " << output
-    << " Current Angle: " << angle.NewData
+    std::cout << 
+    " Current Angle: " << angle.NewData
     << " Target: " << angle.target_angle << "°\n";
 
 
-    int intSteps = static_cast<int>(round(output / 0.08789));
+    // int intSteps = static_cast<int>(round(output / 0.08789));
 
-        if(intSteps > 0)
-        {
-            motor.forward(intSteps);
-        }
-        else if(intSteps < 0)
-        {
-            motor.backward(-intSteps);
-        }
+    //     if(intSteps > 0)
+    //     {
+    //         motor.forward(intSteps);
+    //     }
+    //     else if(intSteps < 0)
+    //     {
+    //         motor.backward(-intSteps);
+    //     }
 
-        time_flag = 0;
-
-
+    //     time_flag = 0;
 
 
-    }
+
+
+    // }
 }
