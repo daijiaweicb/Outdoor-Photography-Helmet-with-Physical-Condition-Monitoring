61526e4866d39127ef52ad0311127fae29df1c73
zyx <2511305125@qq.com>
2025-03-21 16:34:17 +0000

    New Test

--- test/bmp_test.cpp
+++ test/bmp_test.cpp
@@ -1,2 +1,18 @@
 #include <iostream>
+#include "BME280.h"
 
+class BMERealization : public BME,public BME::Callbackinterface
+{
+    public:
+    void BMECallback(BMEresults &results) override
+    {
+        
+    }
+};
+
+
+int main()
+{
+
+    return 0;
+}
