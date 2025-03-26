5eb2bdafe888f2c1c2dacaf598e48c456c8df235
zyx <2511305125@qq.com>
2025-03-26 22:54:41 +0000

    Move start motor  outside of the motor control class

--- include/motor_control.h
+++ include/motor_control.h
@@ -4,35 +4,26 @@
 #include "motor_setting.h"
 #include "MPU6050.h"
 
-class MotorControl : public MPU, public StepperMotor, public MPUCallbackInterface
+class MotorControl : public MPU, public MPU::MPUCallbackInterface, public StepperMotor 
 {
 private:
     StepperMotor motor;
-    struct Data //motor angle control data
+    struct Data // motor angle control data
     {
         float PrevData;
         float NewData;
         float RevData;
-        float target_angle;
+        float target_angle; 
         float integral;
         float prev_error;
     };
     Data angle;
 
 public:
-    void MPUCallback(float& data)override;
+    void MPUCallback(AngleData &data) override;
     MotorControl()
     {
         angle.PrevData = 0;
-        if (motor.start())
-        {
-            std::cout << "motor init success" << std::endl;
-        }
-    }
-
-    ~MotorControl()
-    {
-        motor.cleanup();
     }
 };
 
