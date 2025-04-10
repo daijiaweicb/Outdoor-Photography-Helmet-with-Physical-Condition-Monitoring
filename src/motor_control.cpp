5a4593ec09fd66f1d1076437f2dd07b00acece1e
zyx <2511305125@qq.com>
2025-04-10 16:01:11 +0100

    Add temp

--- src/motor_control.cpp
+++ src/motor_control.cpp
@@ -9,7 +9,7 @@ void MotorControl::MPUCallback(AngleData& data)
     angle.RevData = angle.NewData - angle.PrevData;
 
     // std::cout << "Angle Change: " << angle.RevData << " New Angle: " << angle.NewData << "°" << " tempeature is: " << data.temp << std::endl;
-    std::cout << "Angle Change: " << angle.RevData << " New Angle: " << angle.NewData << "°" <<std::endl;
+    std::cout << "Angle Change: " << angle.RevData << " New Angle: " << angle.NewData << "°" << " Temp: " << data.temp << "℃" <<std::endl;
     if(angle.RevData >= 90)
     {
         motor.setAngle(angle.RevData - 90);
