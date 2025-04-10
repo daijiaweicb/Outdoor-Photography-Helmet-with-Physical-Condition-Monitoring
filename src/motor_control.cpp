7e8d3136b9b05dcaf575419e9dfeff44da8d3b66
zyx <2511305125@qq.com>
2025-04-10 16:04:04 +0100

    Change angle logic

--- src/motor_control.cpp
+++ src/motor_control.cpp
@@ -10,13 +10,13 @@ void MotorControl::MPUCallback(AngleData& data)
 
     // std::cout << "Angle Change: " << angle.RevData << " New Angle: " << angle.NewData << "°" << " tempeature is: " << data.temp << std::endl;
     std::cout << "Angle Change: " << angle.RevData << " New Angle: " << angle.NewData << "°" << " Temp: " << data.temp << "℃" <<std::endl;
-    if(angle.RevData >= 90)
+    if(angle.RevData >= 0)
     {
-        motor.setAngle(angle.RevData - 90);
+        motor.setAngle(90 + angle.RevData);
     }
     else if(angle.RevData < 90)
     {
-        motor.setAngle(angle.RevData);
+        motor.setAngle(90 - angle.RevData);
     }
 
     
