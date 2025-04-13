949da2a82e03081db107297d9c90ccde48f8a2e9
zyx <2511305125@qq.com>
2025-04-13 18:25:15 +0100

    Debug

--- QTViewer/motor_thread.cpp
+++ QTViewer/motor_thread.cpp
@@ -24,7 +24,7 @@ void MotorThread::run()
     } else if (g_systemMode == SystemMode::FatigueDetection) {
         g_systemMode = SystemMode::Temp;
         motor.backward(2048);
-        servo.setAngle(90);
+        servo.start_mg90s();
         QThread::sleep(1);
         g_systemMode = SystemMode::Normal;
     }
