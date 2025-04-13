71639cb14dc665d7469599319f6bbe75e0f44299
zyx <2511305125@qq.com>
2025-04-13 23:46:49 +0100

    Fix the bug of set angle of servo

--- QTViewer/motor_thread.cpp
+++ QTViewer/motor_thread.cpp
@@ -12,6 +12,7 @@ MotorThread::~MotorThread()
 
 void MotorThread::run()
 {
+    servo.start_mg90s(); 
     if (!motor.start(0, 17, 25, 27, 22)) {
         qDebug() << "Stepmotor init failed";
         return;
@@ -19,13 +20,17 @@ void MotorThread::run()
 
     if (g_systemMode == SystemMode::Normal) {
         g_systemMode = SystemMode::Temp;
+        emit modeChanged(g_systemMode);
         motor.forward(2048);
         g_systemMode = SystemMode::FatigueDetection;
+        emit modeChanged(g_systemMode);
     } else if (g_systemMode == SystemMode::FatigueDetection) {
         g_systemMode = SystemMode::Temp;
+        emit modeChanged(g_systemMode);
         motor.backward(2048);
         servo.setAngle(90);
         g_systemMode = SystemMode::Normal;
+        emit modeChanged(g_systemMode);
     }
 
     emit modeChanged(g_systemMode);
