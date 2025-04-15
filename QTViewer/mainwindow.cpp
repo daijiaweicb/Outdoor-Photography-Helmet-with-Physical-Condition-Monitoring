1aa8c9e6fc4ae02781b4720c0059ca2fb3dc61f6
echo <enecholighten@163.com>
2025-04-16 00:52:43 +0100

    Debug without angle detection

--- QTViewer/mainwindow.cpp
+++ QTViewer/mainwindow.cpp
@@ -25,15 +25,15 @@ MainWindow::MainWindow(QWidget *parent)
         QString now = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss");
         ui->label_time->setText("Time:" + now); });
 
-    service = new MotorSensorService();
-    service->start();
-    connect(service->getMotorControl(), &MotorControlQT::temperatureUpdated,
-            this, [=](float temp)
-            { ui->label_temp->setText(QString("Temp: %1 ℃").arg(temp, 0, 'f', 1)); });
+    // service = new MotorSensorService();
+    // service->start();
+    // connect(service->getMotorControl(), &MotorControlQT::temperatureUpdated,
+    //         this, [=](float temp)
+    //         { ui->label_temp->setText(QString("Temp: %1 ℃").arg(temp, 0, 'f', 1)); });
 
-    connect(service->getMotorControl(), &MotorControlQT::angleUpdate,
-            this, [=](float angle)
-            { ui->label_angle->setText(QString("Angle: %1°").arg(angle, 0, 'f', 1)); });
+    // connect(service->getMotorControl(), &MotorControlQT::angleUpdate,
+    //         this, [=](float angle)
+    //         { ui->label_angle->setText(QString("Angle: %1°").arg(angle, 0, 'f', 1)); });
 
     timer->start(1000);
 
