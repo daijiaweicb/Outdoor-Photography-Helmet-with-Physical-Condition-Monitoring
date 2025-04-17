530a4bd022f2ea690b03473b611ea370b2bf2ade
zyx <2511305125@qq.com>
2025-04-17 11:44:14 +0100

    optimise the ui

--- QTViewer/mainwindow.cpp
+++ QTViewer/mainwindow.cpp
@@ -52,8 +52,7 @@ MainWindow::MainWindow(QWidget *parent)
     QString imagePath = "../../QTViwer/images/background.png";
     QPixmap pix(imagePath);
 
-    if (!pix.isNull())
-    {
+    if (!pix.isNull()) {
         QStandardItemModel *model = new QStandardItemModel(this);
         QStandardItem *item = new QStandardItem();
         item->setIcon(QIcon(pix));
@@ -62,13 +61,10 @@ MainWindow::MainWindow(QWidget *parent)
 
         ui->listView->setModel(model);
         ui->listView->setViewMode(QListView::IconMode);
-        ui->listView->setMovement(QListView::Static); 
-        ui->listView->setSpacing(0);                  
-        ui->listView->setIconSize(ui->listView->size());
+        ui->listView->setIconSize(QSize(150, 150));
+        ui->listView->setSpacing(0);
         ui->listView->setResizeMode(QListView::Adjust);
-    }
-    else
-    {
+    } else {
         qDebug() << "Background load failed" << imagePath;
     }
 }
