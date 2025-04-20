9bdceee0029ab993d7eb028022f633839b491956
zyx <2511305125@qq.com>
2025-04-20 23:50:24 +0100

    Try to fix the bug of the timer can only start one time

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -15,8 +15,8 @@ void HighPrecisionTimer::start(int millisecs, Callback cb) {
     //Calculate cycle interval
     its.it_value.tv_sec = sec;
     its.it_value.tv_nsec = nsec;
-    its.it_interval.tv_sec = 0;
-    its.it_interval.tv_nsec = 0;
+    its.it_interval.tv_sec = sec;
+    its.it_interval.tv_nsec = nsec;
 
 
     if (timerfd_settime(fd, 0, &its, nullptr) == -1) {
