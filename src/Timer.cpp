b6f60862556e583663e4ca30344ac396c6a3f24e
zyx <2511305125@qq.com>
2025-04-21 01:11:20 +0100

    Use gdb to find the issue is in timer.cpp 33

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,16 +1,16 @@
 #include "Timer.h"
 /**
  * HighPrecisionTimer. start the timer
- * 
- * @param  {int} millisecs : 
- * @param  {Callback} cb   : 
+ *
+ * @param  {int} millisecs :
+ * @param  {Callback} cb   :
  */
-void HighPrecisionTimer::start(int millisecs, Callback cb) {
-    if (running) {
+void HighPrecisionTimer::start(int millisecs, Callback cb)
+{
+    if (running)
+    {
         stop();
     }
-
-    running = true;
 
     struct itimerspec its;
     const int sec = millisecs / 1000;
@@ -21,59 +21,73 @@ void HighPrecisionTimer::start(int millisecs, Callback cb) {
     its.it_interval.tv_sec = sec;
     its.it_interval.tv_nsec = nsec;
 
-    if (timerfd_settime(fd, 0, &its, nullptr) == -1) {
+    if (timerfd_settime(fd, 0, &its, nullptr) == -1)
+    {
         throw std::runtime_error("timerfd_settime failed: " + std::string(strerror(errno)));
     }
 
     running = true;
 
-    worker = std::thread([this, cb]() {
+    worker = std::thread([this, cb]()
+                         {
         setThreadPriority();
-        eventLoop(cb);
-    });
+        eventLoop(cb); });
 }
 
 /**
  * Stop the timer
  */
-void HighPrecisionTimer::stop() {
-    if (!running.exchange(false)) return;
+void HighPrecisionTimer::stop()
+{
+    if (!running.exchange(false))
+        return;
 
     struct itimerspec its{};
     timerfd_settime(fd, 0, &its, nullptr);
 
-    if (worker.joinable() && std::this_thread::get_id() != worker.get_id()) {
+    if (worker.joinable() && std::this_thread::get_id() != worker.get_id())
+    {
         worker.join();
     }
+    worker = std::thread();
 }
 
+
 /**
- * HighPrecisionTimer 
- * 
- * @param  {Callback} cb : 
+ * HighPrecisionTimer
+ *
+ * @param  {Callback} cb :
  */
-void HighPrecisionTimer:: eventLoop(const Callback& cb) {
+void HighPrecisionTimer::eventLoop(const Callback &cb)
+{
     constexpr size_t buf_size = sizeof(uint64_t);
     uint64_t exp;
 
-    while (running) {
+    while (running)
+    {
         ssize_t bytes = read(fd, &exp, buf_size);
-        
+
         // signal interrupt
-        if (bytes == -1 && errno == EINTR) {
+        if (bytes == -1 && errno == EINTR)
+        {
             continue;
         }
 
-        if (bytes != buf_size) {
+        if (bytes != buf_size)
+        {
             std::cerr << "Timer read error: " << strerror(errno) << std::endl;
             break;
         }
 
         // calback
-        if (exp > 0 && cb) {
-            try {
+        if (exp > 0 && cb)
+        {
+            try
+            {
                 cb();
-            } catch (const std::exception& e) {
+            }
+            catch (const std::exception &e)
+            {
                 std::cerr << "Timer callback error: " << e.what() << std::endl;
             }
         }
@@ -82,12 +96,14 @@ void HighPrecisionTimer:: eventLoop(const Callback& cb) {
 /**
  * Setting thread priority
  */
-void HighPrecisionTimer:: setThreadPriority() {
+void HighPrecisionTimer::setThreadPriority()
+{
     struct sched_param param;
     param.sched_priority = sched_get_priority_max(SCHED_FIFO);
-    
-    if (pthread_setschedparam(pthread_self(), SCHED_FIFO, &param) != 0) {
-        std::cerr << "Warning: Failed to set realtime priority (" 
+
+    if (pthread_setschedparam(pthread_self(), SCHED_FIFO, &param) != 0)
+    {
+        std::cerr << "Warning: Failed to set realtime priority ("
                   << strerror(errno) << ")" << std::endl;
     }
 }
